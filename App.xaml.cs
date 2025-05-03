3fa45d0804718836841b9dfc8fc6d6304b925796
Margarida Santini Szabo <margarida.szabo@alumni.usp.br>
2025-05-03 11:31:53 -0300

    Atualizações

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,8 +6,31 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            string? usuario_logado = null;
+
+            MainPage = new Login();
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+
+                if (usuario_logado != null)
+                {
+                    MainPage = new Protegida();
+                }
+
+                /*if(usuario_logado == null)
+                {
+                    MainPage = new Login();
+
+                } else
+                {
+                    MainPage = new Protegida();
+                }*/
+            });
         }
+
+
         protected override Window CreateWindow(IActivationState activationState)
         {
             var window = base.CreateWindow(activationState);
@@ -19,4 +42,4 @@
         }
 
     } // Fecha classe 
-} // Fecha namespace
+} // Fecha namespace 
--- Login.xaml.cs
+++ Login.xaml.cs
@@ -6,4 +6,46 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			List<DadosUsuario> lista_usuarios = new List<DadosUsuario>()
+			{
+				new DadosUsuario()
+				{
+					Usuario = "Jose",
+					Senha = "123"
+				},
+				new DadosUsuario()
+				{
+					Usuario = "Maria",
+					Senha = "321"
+				}
+			};
+
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+
+			//LINQ
+			if (lista_usuarios.Any(
+				i => (dados_digitados.Usuario == i.Usuario &&
+				dados_digitados.Senha == i.Senha)))
+			{
+				SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+				App.Current.MainPage = new Protegida();
+			}
+
+
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "Fechar");
+		}
+	}
 }
